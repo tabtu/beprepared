ccc234cf32fe2a92775a9ac05ff98197592eb86c
Tab Tu <i@tabtu.cn>
2021-12-28 16:54:55 -0500

    solved The Minion Game, Time Delta, Any or All soluation.

---
+++ Python/Any or All.py
@@ -0,0 +1,6 @@
+# https://www.hackerrank.com/challenges/any-or-all
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+N, n = int(input()), input().split(' ')
+print(all([int(i) > 0 for i in n]) and any([j == j[::-1] for j in n]))
--- Python/Wriite a function.py
+++ Python/Wriite a function.py
@@ -1,3 +1,5 @@
+# https://www.hackerrank.com/challenges/write-a-function
+
 def is_leap(year):
     leap = False
     # Write your logic here
@@ -8,6 +10,7 @@ def is_leap(year):
         if year % 400 == 0:
             leap = True
     return leap
+
 
 year = int(input())
 print(is_leap(year))
