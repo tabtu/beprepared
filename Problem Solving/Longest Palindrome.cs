205e9351868a7fd44d803bbcfbdd37b87fb4a691
tabtu <i@tabtu.cn>
2021-12-31 19:06:51 -0500

    add a based method on longest palindrome.

--- Problem Solving/Longest Palindrome.cs
+++ Problem Solving/Longest Palindrome.cs
@@ -27,4 +27,28 @@ public class Solution
         carry = (left == right) ? (s[left] + carry) : (s[left] + carry + s[right]);
         return GetPalindrome(left - 1, right + 1, s, carry);
     }
+
+    public static string longestPalindrome_0(string s)
+    {
+        if (s.Length < 2) return s;
+        string res = "";
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            string s1 = palindrome(s, i, i);
+            string s2 = palindrome(s, i, i + 1);
+            res = res.Length > s1.Length ? res : s1;
+            res = res.Length > s2.Length ? res : s2;
+        }
+        return res;
+    }
+
+    public static string palindrome(string s, int left, int right)
+    {
+        while (left > -1 && right < s.Length && s[left] == s[right])
+        {
+            left--;
+            right++;
+        }
+        return s.Substring(left + 1, right - left - 1);
+    }
 }
