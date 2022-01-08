12d8ca708208ec0046cc1673e312707b1ff5ec7c
Tab Tu <i@tabtu.cn>
2022-01-07 19:51:46 -0500

    training on one way stack, reviewing BST continuly

--- Problem Solving/Absolute Permutation.cs
+++ Problem Solving/Absolute Permutation.cs
@@ -18,7 +18,9 @@ class Result
     /*
      * https://www.hackerrank.com/challenges/absolute-permutation
      * 
-     * 
+     * double queue/list problem. 
+     * - special with n < 3 && k < 2
+     * - if ()
      * 
      * Complete the 'absolutePermutation' function below.
      *
---
+++ Problem Solving/Daily Temperatures.cs
@@ -0,0 +1,36 @@
+/* https://leetcode.com/problems/daily-temperatures/submissions/
+ * 
+ * Single way stack, only push element value in one way.
+ * - push in stack one by one;
+ * - loop pop when t[cur] > t[stack.peek], or empty stack
+ * 
+ * stack saves the position in array.
+ */
+public class Solution
+{
+
+    public void test_dailyTemperatures()
+    {
+        int[] t = { 73, 74, 75, 71, 69, 72, 76, 73 };
+        int[] r = dailyTemperatures(t);
+        // foreach (int i in r)
+        // {
+        //     Console.Write(i + " ");
+        // }
+    }
+
+    public int[] DailyTemperatures(int[] temperatures)
+    {
+        int[] ans = new int[temperatures.Length];
+        Stack<int> s = new Stack<int>();
+        for (int i = 0; i < temperatures.Length; i++)
+        {
+            while (s.Count() > 0 && temperatures[i] > temperatures[s.Peek()])
+            {
+                ans[s.Peek()] = i - s.Pop();
+            }
+            s.Push(i);
+        }
+        return ans;
+    }
+}
